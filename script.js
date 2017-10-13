9a0dd6d52a60028b8e06fee1649c5f751d8845d5
Mirtau <tjj72@comcast.net>
2017-10-12 21:41:43 -0600

    checkerboard working

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+for (var i = 0; i < 81; i++) {
+  var tile = document.createElement('div');
+  var body = document.getElementsByTagName('body')[0];
+
+  tile.style.width = '11.1%';
+  tile.display = 'float';
+  tile.style.float = 'left';
+  tile.style.paddingBottom = '11.1%';
+
+  if (i % 2 === 0) {
+    tile.style.background = 'color';
+  } else {
+    tile.style.background = 'color';
+  }
+
+  body.appendChild(tile);
+}
